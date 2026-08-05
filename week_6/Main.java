bb7cc5969e863c145d76cc905c126bc64e1fe351
pzqmm <taon9100@gmail.com>
2026-08-05 18:43:08 +0000

    Completed week 6

---
+++ week_6/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Lecturer lecturer = new Lecturer("L100", "Dr Ahmad", "Java Programming", "Faculty of Information Technology");
+
+        lecturer.displayInfo();
+        lecturer.displaySubject();
+    }
+}
