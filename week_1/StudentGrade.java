34441364410abec7a03c7a8c4ad735079abfdc8e
pzqmm <taon9100@gmail.com>
2026-08-05 18:01:45 +0000

    Completed week 1: Added HelloWorld and StudentGrade

---
+++ week_1/StudentGrade.java
@@ -0,0 +1,21 @@
+public class StudentGrade {
+    public static void main(String[] args) {
+        int score = 85; 
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        
+        System.out.println("Student Score: " + score);
+        System.out.println("Student Grade: " + grade);
+    }
+}
