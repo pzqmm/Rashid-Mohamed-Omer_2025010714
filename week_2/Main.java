589bd8c65c21fa6905366b3fb3caff668f05e169
pzqmm <taon9100@gmail.com>
2026-08-05 18:10:51 +0000

    Completed week 2: Added Student and Main classes

---
+++ week_2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // coding starts here;
+        
+        Student s1 = new Student("Ahmad", 20, 3.8);
+        
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
